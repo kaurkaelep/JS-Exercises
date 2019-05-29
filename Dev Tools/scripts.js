e113fc8d8d2c75523c0c70c6b4c09a2983d56344
Kaur Kaelep <kaur.kaelep@gmail.com>
2019-05-29 08:51:37 +0300

    Excercises

---
+++ Dev Tools/scripts.js
@@ -0,0 +1,54 @@
+const dogs = [{ name: "Snickers", age: 2 }, { name: "hugo", age: 8 }];
+
+function makeGreen() {
+  const p = document.querySelector("p");
+  p.style.color = "#BADA55";
+  p.style.fontSize = "50px";
+}
+
+// Regular
+console.log("hello");
+// Interpolated
+console.log("Hello I am a %s string!", "poop");
+// Styled
+console.log(
+  "%c I am some great text",
+  "font-size:50px; background: lightblue;"
+);
+// warning!
+console.warn("OH NOOO");
+// Error :|
+console.error("Shit!");
+// Info
+console.info("Crocodiles eat 3-4 people per year");
+// Testing
+const p = document.querySelector("p");
+console.assert(p.classList.contains("ouch"), "That is wrong!");
+// clearing
+console.clear();
+// Viewing DOM Elements
+console.log(p);
+console.dir(p);
+// Grouping together
+dogs.forEach(dog => {
+  console.log(`This is ${dog.name}`);
+});
+// counting
+console.count("Cos");
+console.count("Cos");
+console.count("Wes");
+console.count("Wes");
+console.count("Bos");
+console.count("Bos");
+console.count("Bos");
+console.count("Bos");
+console.count("Bos");
+console.count("Wes");
+// timing
+console.time("fetching data");
+fetch("https://api.github.com/users/kaurkaelepkhk")
+  .then(data => data.json())
+  .then(data => {
+    console.timeEnd("fetching data");
+    console.log(data);
+  });
